e80a171c1dd1aef81b1a870148f36ee0e7c93e82
PixelAmp <reperez79@gmail.com>
2017-11-19 15:45:41 -0800

    Added new pag to display current log
    new page doesn't display anything yet, but we can navigate to it

--- BlueSheep Mobile/Sample/Sample.Shared/MainPage.xaml.cs
+++ BlueSheep Mobile/Sample/Sample.Shared/MainPage.xaml.cs
@@ -11,5 +11,11 @@ namespace BlueSheep
             InitializeComponent();
             this.BindingContext = new MainViewModel();
         }
+
+        void GoToLogPage(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new LogDisplay());
+        }
+
     }
 }
